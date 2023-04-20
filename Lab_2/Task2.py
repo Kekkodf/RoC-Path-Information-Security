bd00d4212bb24421c7e30295d394fbf8a97bf753
kdf_7 <109605653+Kekkodf@users.noreply.github.com>
2023-04-20 13:30:47 +0200

    Add files via upload

--- Lab_2/Task2.py
+++ Lab_2/Task2.py
@@ -20,25 +20,6 @@ def hamming_distance(v1, v2):
 message_space = [[0,0,0], [0,0,1], [0,1,0], [1,0,0],
                  [0,1,1], [1,0,1], [1,1,0], [1,1,1]]
 
-def areEqual(arr1, arr2, N, M):
- 
-    # If lengths of array are not
-    # equal means array are not equal
-    if (N != M):
-        return False
- 
-    # Sort both arrays
-    arr1.sort()
-    arr2.sort()
- 
-    # Linearly compare elements
-    for i in range(0, N):
-        if (arr1[i] != arr2[i]):
-            return False
- 
-    # If all elements were same.
-    return True
-
 #uniform binning encoder
 def uniform_binning_encoder(m):
     y = []
@@ -49,21 +30,28 @@ def uniform_binning_encoder(m):
         return
     else:
         for x in cipher_space:
-            if areEqual(x[1:4], m, len(x[1:4]), len(m)):
-                y = x
-                break
+            if x[0] == 0:
+                if x[1:4] == m:
+                    y = x
+                    break
     #computing the complemnetary code
     for i in range(len(y)):
         if y[i] == 0:
             z.append(1)
         else:
             z.append(0)
-    return y, z
+    set = [y, z]
+    return set
+
+def choose_random_x(set):
+    return random.choice(set)
 
 def main():
-    m = [1,0,0]
+    m = [1,0,1]
     print("The input is", m)
-    print("The output is", uniform_binning_encoder(m))
+    print("The bin based on the input is", uniform_binning_encoder(m))
+    print("The random x is", choose_random_x(uniform_binning_encoder(m)))
+
 
 if __name__ == "__main__":
     main()
