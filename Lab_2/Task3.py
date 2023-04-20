64dbdcfefd346850ae8b027daa5ef620345ad4ed
kdf_7 <109605653+Kekkodf@users.noreply.github.com>
2023-04-20 17:58:53 +0200

    Add files via upload

--- Lab_2/Task3.py
+++ Lab_2/Task3.py
@@ -11,6 +11,12 @@ cipher_space = [[0,0,0,0,0,0,0], [1,0,0,0,1,1,0], [0,1,0,0,1,0,1], [0,0,1,0,0,1,
 
 def uniform_binning_decoder(c):
     #compute the opposite of the input
+    for element in cipher_space:
+        element = np.array(element)
+        hamming_d = t2.hamming_distance(c, element)
+        if hamming_d == 1:
+            c = element
+            break
     c_opposite = []
     m = []
     for i in range(len(c)):
@@ -18,16 +24,12 @@ def uniform_binning_decoder(c):
             c_opposite.append(1)
         else:
             c_opposite.append(0)
-    if (c in cipher_space or c_opposite in cipher_space):
-        if c[0] == 0:
-            m = c[1:4]
-            return m
-        if c[0] == 1:
-            m = c_opposite[1:4]
-            return m
-    else:
-        print("The input is not in the cipher space")
-        return
+    if c[0] == 0:
+        m = c[1:4]
+        return m
+    if c[0] == 1:
+        m = c_opposite[1:4]
+        return m
 
 def main():
     c = [1, 0, 1, 1, 0, 1, 0]
@@ -42,7 +44,8 @@ def main():
     print("")
     #proove that the decoder is correct with the legitimate channel
     print("Proof that the decoder is correct with the legitimate channel. (Encoding + Legitimate Channel + Decoding)")
-
+    print("Encoding the output of the decoder:", t2.uniform_binning_encoder(uniform_binning_decoder(c)))
+    print("Legitimate Channel:", uniform_binning_decoder(t1.legitimate_corruption(t2.uniform_binning_encoder(m))))
 
 if __name__ == "__main__":
     main()
