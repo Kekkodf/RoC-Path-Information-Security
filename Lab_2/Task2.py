63c2a46ef3226893ae5b68958d16931b2edc07d1
Luca <luca.cosuti@outlook.com>
2023-04-24 13:14:16 +0200

    Update Task2.py

--- Lab_2/Task2.py
+++ Lab_2/Task2.py
@@ -2,60 +2,91 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-cipher_space = [[0,0,0,0,0,0,0], [1,0,0,0,1,1,0], [0,1,0,0,1,0,1], [0,0,1,0,0,1,1], [0,0,0,1,1,1,1],
-                [1,1,0,0,0,1,1], [1,0,1,0,1,0,1], [1,0,0,1,0,0,1], [0,1,1,0,1,1,0], [0,1,0,1,0,1,0], 
-                [0,0,1,1,1,0,0], [1,1,1,0,0,0,0], [1,1,0,1,1,0,0], [1,0,1,1,0,1,0], [0,1,1,1,0,0,1],
-                [1,1,1,1,1,1,1]]
+cipher_space = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 1, 1, 0],
+    [0, 1, 0, 0, 1, 0, 1],
+    [0, 0, 1, 0, 0, 1, 1],
+    [0, 0, 0, 1, 1, 1, 1],
+    [1, 1, 0, 0, 0, 1, 1],
+    [1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 0, 1, 0, 0, 1],
+    [0, 1, 1, 0, 1, 1, 0],
+    [0, 1, 0, 1, 0, 1, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [1, 1, 1, 0, 0, 0, 0],
+    [1, 1, 0, 1, 1, 0, 0],
+    [1, 0, 1, 1, 0, 1, 0],
+    [0, 1, 1, 1, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1],
+]
 
-#compute the Hamming distance between two vectors
+
+# compute the Hamming distance between two vectors
 def hamming_distance(v1, v2):
     return np.sum(np.abs(v1 - v2))
 
-#observe that the minimum Hamming distance in the code is 3
-#for v1 in cipher_space:
+
+# observe that the minimum Hamming distance in the code is 3
+# for v1 in cipher_space:
 #    for v2 in cipher_space:
 #        if not (v1 == v2):
 #            print("Hamming distance between", v1, "and", v2, "is", hamming_distance(np.array(v1), np.array(v2)))
 
-message_space = [[0,0,0], [0,0,1], [0,1,0], [1,0,0],
-                 [0,1,1], [1,0,1], [1,1,0], [1,1,1]]
+message_space = [
+    [0, 0, 0],
+    [0, 0, 1],
+    [0, 1, 0],
+    [0, 1, 1],
+    [1, 0, 0],
+    [1, 0, 1],
+    [1, 1, 0],
+    [1, 1, 1],
+]
 
-#uniform binning encoder
+
+def pmd(x, y):
+    # verify that the inputs are in the cipher space
+    if not (x in cipher_space and y in cipher_space):
+        print("The inputs are not in the cipher space")
+        return
+    else:
+        # compute the Hamming distance between x and y
+        return hamming_distance(np.array(x), np.array(y)) / 7
+
+
+# uniform binning encoder
 def uniform_binning_encoder(m):
     y = []
     z = []
-    #verify that the input is in the cipher space
+    # verify that the input is in the cipher space
     if not (m in message_space):
         print("The input is not in the message space")
-        return
+        return None
     else:
         for x in cipher_space:
             if x[0] == 0:
                 if x[1:4] == m:
                     y = x
+                    print("Exiting")
                     break
-    #computing the complemnetary code
+    # computing the complementary code
     for i in range(len(y)):
         if y[i] == 0:
             z.append(1)
         else:
             z.append(0)
     set = [y, z]
+    print(set)
     ris = random.choice(set)
     return ris
 
+
 def main():
-  for m in message_space:
+    m = [1, 0, 0]
     print("The input is", m)
     print("The random x is", uniform_binning_encoder(m))
-    if uniform_binning_encoder(m) in cipher_space :
-      print('Valid encoding')
-    else:
-      print('Invalid encoding')
-    print(' ')
 
 
 if __name__ == "__main__":
     main()
-
-
