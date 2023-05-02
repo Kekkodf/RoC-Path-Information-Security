ce66b846859a765db09bc65daa1fca990c4b019e
Luca <luca.cosuti@outlook.com>
2023-05-02 18:13:51 +0200

    Update Task5.py

--- Lab_2/Task5.py
+++ Lab_2/Task5.py
@@ -6,34 +6,38 @@ import matplotlib.pyplot as plt
 import random
 from matplotlib import pyplot
 
-#eavsdropper channel corruption example (0 < epsilon < delta < 1/2)
+# eavsdropper channel corruption example (0 < epsilon < delta < 1/2)
 epsilon = 0.1
 delta = 0.2
 
-#legitimate channel corruption example (0 < delta < epsilon < 1/2)
-#epsilon = 0.2
-#delta = 0.1
+# legitimate channel corruption example (0 < delta < epsilon < 1/2)
+# epsilon = 0.2
+# delta = 0.1
 
-#define the number of trials
-number_trials = 100000
+# define the number of trials
+number_trials = 10000
+
 
 def BSC(epsilon, delta, m):
-    if (abs((epsilon - 0.5))<abs(delta - 0.5)):
+    if abs((epsilon - 0.5)) < abs(delta - 0.5):
+        print("Encoded: ", t2.uniform_binning_encoder(m))
         v = t1.legitimate_corruption(t2.uniform_binning_encoder(m))
         return v
-    if (abs(epsilon - 0.5)>abs(delta - 0.5)):
-        v = t1.eavesdropper_corruption(t2.uniform_binning_encoder(m))
+    if abs(epsilon - 0.5) > abs(delta - 0.5):
+        print("Encoded: ", t2.uniform_binning_encoder(m))
+        v = t1.limited_corruption(t2.uniform_binning_encoder(m))
         return v
     else:
         return None
 
+
 def main():
-    print("If 0 < delta < epsilon < 0.5") #legitimate channel is degraded
-    print("If 0 < epsilon < delta < 0.5") #eavesdropper channel is degraded
+    print("If 0 < delta < epsilon < 0.5")  # legitimate channel is degraded
+    print("If 0 < epsilon < delta < 0.5")  # eavesdropper channel is degraded
     print("Epsilon is set to:", epsilon)
     print("Delta is set to:", delta)
     print("")
-    #create a random message
+    # create a random message
     count_correctly_matched = 0
     count_missmatched = 0
     for i in range(number_trials):
@@ -60,17 +64,21 @@ def main():
     print("The number of correctly matched messages is:", count_correctly_matched)
     print("The number of missmatched messages is:", count_missmatched)
 
-    #plot the results in a barh graph
-    objects = ('Correctly decoded', 'Missmatched')
+    # plot the results in a barh graph
+    objects = ("Correctly decoded", "Missmatched")
     y_pos = np.arange(len(objects))
-    performance = [count_correctly_matched/number_trials, count_missmatched/number_trials]
-    plt.barh(y_pos, performance, align='center', alpha=0.5)
+    performance = [
+        count_correctly_matched / number_trials,
+        count_missmatched / number_trials,
+    ]
+    plt.barh(y_pos, performance, align="center", alpha=0.5)
     plt.yticks(y_pos, objects)
-    plt.xlabel('Percentage of messages')
+    plt.xlabel("Percentage of messages")
     plt.show()
-    
-    #save as png
-    plt.savefig('Task5_leg_channel_corruption.png')
+
+    # save as png
+    plt.savefig("Task5_leg_channel_corruption.png")
+
 
 if __name__ == "__main__":
     main()
